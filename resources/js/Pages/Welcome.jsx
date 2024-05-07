821d06385c74da6d0d878b5aca1208f430984da3
Chee3se <emilspete999@gmail.com>
2024-05-08 00:03:18 +0300

    Initial commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,62 @@
+import { Link, Head } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo.jsx";
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const handleImageError = () => {
+        document.getElementById('screenshot-container')?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document.getElementById('docs-card-content')?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
+
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="bg-gray-50 text-black/50">
+                <div className="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                    <div className="relative w-full h-screen max-w-2xl px-6 lg:max-w-7xl">
+                        <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                            <div className="flex lg:justify-center lg:col-start-2">
+                                <ApplicationLogo className={"h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]"} />
+                            </div>
+                            <nav className="-mx-3 flex flex-1 justify-end">
+                                {auth.user ? (
+                                    <Link
+                                        href={route('dashboard')}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                    >
+                                        Dashboard
+                                    </Link>
+                                ) : (
+                                    <>
+                                        <Link
+                                            href={route('login')}
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                        >
+                                            Log in
+                                        </Link>
+                                        <Link
+                                            href={route('register')}
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                        >
+                                            Register
+                                        </Link>
+                                    </>
+                                )}
+                            </nav>
+                            <h1 className="justify-self-center col-start-2">Welcome!</h1>
+                        </header>
+
+                        <main className="mt-6">
+
+                        </main>
+
+                        <footer className="relative bottom-0 w-max mx-auto col-start-2 py-3 text-sm text-black">
+                            Copyright © 2024 You copy, You gay
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
